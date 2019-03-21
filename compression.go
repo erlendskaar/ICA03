197132748ebaca02b5414eb04da286261c1afb91
erlendskaar <46562949+erlendskaar@users.noreply.github.com>
2019-03-21 12:06:32 +0100

    First commit

---
+++ compression.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/erlendskaar/is105-ica03/misc"
+)
+
+func main() {
+	//Kode for funksjoner her, som blir importert fra pipe.go
+
+
+
+	// Her kommer stdout som blir en fmt.Println.
+}
